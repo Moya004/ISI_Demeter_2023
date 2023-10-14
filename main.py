51081bea32d2c426cbc4ac56f327185bd87fb358
Mario Moya <mmartinezm10@unicartagena.edu.co>
2023-10-14 10:25:22 -0500

    First version of the Login view

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 
 
-class MainWidget(Widget):
+class LogInFloatLayout(FloatLayout):
+
+    def login_click(self) -> None:
+        print('Clicked')
+
+
+class BackgroundColor(Widget):
+    pass
+
+
+class BgLabel(Label, BackgroundColor):
     pass
 
 
