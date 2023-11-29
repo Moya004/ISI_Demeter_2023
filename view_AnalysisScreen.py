543c7bc1bf34ad768a6665f8d8e541deb78d5d26
Viviana Prasca <vivianaprascamendez@gmail.com>
2023-11-29 01:13:49 -0500

    Funciones para la vista de analisis de la app

---
+++ view_AnalysisScreen.py
@@ -0,0 +1,19 @@
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+
+class AnalysisScreen(FloatLayout):
+    pass
+
+class BackgroundColor(Widget):
+    pass
+
+class BgLabel(Label, BackgroundColor):
+    pass
+
+class BgCanvas(Widget):
+    pass
+
+Builder.load_file('AnalysisScreen.kv')
