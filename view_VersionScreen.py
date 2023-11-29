b2e4b9db83088de7ad8f7b2509ce9013181ff2df
Viviana Prasca <vivianaprascamendez@gmail.com>
2023-11-29 01:05:31 -0500

    Funciones para la vista de version de demeter

---
+++ view_VersionScreen.py
@@ -0,0 +1,25 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+class VersionScreen(FloatLayout):
+    pass
+
+class BackgroundColor(Widget):
+    pass
+
+class BgLabel(Label, BackgroundColor):
+    pass
+
+class BgCanvas(Widget):
+    pass
+
+class DropDown_Year3(BoxLayout):
+    pass
+
+class DropDown_Mes3(BoxLayout):
+    pass
+
+Builder.load_file('VersionScreen.kv')
