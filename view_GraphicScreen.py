afa7e15917829d18e4c7f5c8a9693285b7eb2cee
Viviana Prasca <vivianaprascamendez@gmail.com>
2023-11-29 01:12:28 -0500

    Funciones para la vista de graficas de la app

---
+++ view_GraphicScreen.py
@@ -0,0 +1,29 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+
+class GraphicScreen(FloatLayout):
+    pass
+
+class BackgroundColor(Widget):
+    pass
+
+class BgLabel(Label, BackgroundColor):
+    pass
+
+class BgCanvas(Widget):
+    pass
+
+class CustomDropdown(FloatLayout):
+    pass
+
+class DropDown_Year2(BoxLayout):
+    pass
+
+class DropDown_Mes2(BoxLayout):
+    pass
+
+Builder.load_file('GraphicScreen.kv')
