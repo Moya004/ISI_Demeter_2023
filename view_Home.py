73406b1509c7263e689e4af0dfca5101df8ddad2
Viviana Prasca <vivianaprascamendez@gmail.com>
2023-11-29 01:11:41 -0500

    Funciones para la vista de inicio de la app

---
+++ view_Home.py
@@ -0,0 +1,21 @@
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
+
+class HomeScreen(FloatLayout):
+    pass
+
+class BackgroundColor(Widget):
+    pass
+
+class BgLabel(Label, BackgroundColor):
+    pass
+
+
+class BgCanvas(Widget):
+    pass
+
+
+Builder.load_file('HomeScreen.kv')
