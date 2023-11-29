c17c447a1d3ab2fe222baa4efe9d17351eb90ecf
Viviana Prasca <vivianaprascamendez@gmail.com>
2023-11-29 01:14:41 -0500

    Funciones para la vista de alertas de la app

---
+++ view_AlertScreen.py
@@ -0,0 +1,25 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+class AlertScreen(FloatLayout):
+    pass
+
+class BackgroundColor(Widget):
+    pass
+
+class BgLabel(Label, BackgroundColor):
+    pass
+
+class BgCanvas(Widget):
+    pass
+
+class DropDown_Year(BoxLayout):
+    pass
+
+class DropDown_Mes(BoxLayout):
+    pass
+
+Builder.load_file('AlertsScreen.kv')
